496997e3930503ffe59272a16dd5564964055dd1
91798 <91798@DESKTOP-F2FI2LC>
2022-08-21 21:50:26 +0530

    V-1

---
+++ mavenproj/src/main/java/com/mavenproj/mavenproj/App.java
@@ -0,0 +1,13 @@
+package com.mavenproj.mavenproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
